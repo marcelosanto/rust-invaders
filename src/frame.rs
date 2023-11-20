ffc4dbbb74778cf4410b9fa2c98c0c8a5d765b0f
Marcelo Santos <rocha.mer21@gmail.com>
2023-11-20 19:00:28 -0300

    new_frame

---
+++ src/frame.rs
@@ -0,0 +1,23 @@
+use crate::{NUM_COLS, NUM_ROWS};
+
+pub type Frame = Vec<Vec<&'static str>>;
+
+pub fn new_frame() -> Frame {
+    let mut cols = Vec::with_capacity(NUM_COLS);
+
+    for _ in 0..NUM_COLS {
+        let mut col = Vec::new();
+
+        for _ in 0..NUM_ROWS {
+            col.resize(NUM_ROWS, " ");
+        }
+
+        cols.push(col);
+    }
+
+    cols
+}
+
+pub trait Drawable {
+    fn draw(&self, frame: &mut Frame);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::error::Error;
+use std::{error::Error, io, time::Duration};
 
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -14,8 +20,44 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game loop
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    event::KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    },
+
+                    event::KeyCode::Enter => todo!(),
+
+                    event::KeyCode::Left | KeyCode::Char('a') => todo!(),
+
+                    event::KeyCode::Right | KeyCode::Char('d') => todo!(),
+
+                    event::KeyCode::Up | KeyCode::Char('w') => todo!(),
+
+                    event::KeyCode::Down | KeyCode::Char('s') => todo!(),
+
+                    _ => todo!(),
+                }
+            }
+        }
+    }
+
     //Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
 
     Ok(())
 }
